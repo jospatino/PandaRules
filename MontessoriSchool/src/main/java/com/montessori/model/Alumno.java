e01daf4fa88b0eb258d3ba9bd59955323e45a8f2
Geovanni Perez <ivan.gpv@gmail.com>
2021-03-29 12:56:17 -0600

    Cambios properties

--- MontessoriSchool/src/main/java/com/montessori/model/Alumno.java
+++ MontessoriSchool/src/main/java/com/montessori/model/Alumno.java
@@ -1,6 +1,22 @@
 package com.montessori.model;
 
-public class Alumno {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-	//Hola desde Git, jeje
+@Entity
+@Table(name = "tb_alumno")
+public class Alumno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idAlumno")
+	private int idAlumno;
+	
+	@Column(name = "nombre_alumno", length = 30, nullable = false)
+	private String nombreAl;
+
 }
