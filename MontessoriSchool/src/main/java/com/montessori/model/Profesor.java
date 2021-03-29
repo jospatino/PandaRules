2344eb74d9c696b1d67e8d7f3046965927ccec7e
DiegoViag <diegovielma14@gmail.com>
2021-03-29 14:43:23 -0600

    Cambios a Profesor

--- MontessoriSchool/src/main/java/com/montessori/model/Profesor.java
+++ MontessoriSchool/src/main/java/com/montessori/model/Profesor.java
@@ -52,32 +52,28 @@ public class Profesor {
 		return idProf;
 	}
 
-	public void setIdProf(int idProf) {
-		this.idProf = idProf;
+	public String getNomProf() {
+		return nomProf;
 	}
 
-	public String getNom_prof() {
-		return nom_prof;
+	public void setNomProf(String nomProf) {
+		this.nomProf = nomProf;
 	}
 
-	public void setNom_prof(String nom_prof) {
-		this.nom_prof = nom_prof;
+	public String getAppProf() {
+		return appProf;
 	}
 
-	public String getApp_prof() {
-		return app_prof;
+	public void setAppProf(String appProf) {
+		this.appProf = appProf;
 	}
 
-	public void setApp_prof(String app_prof) {
-		this.app_prof = app_prof;
+	public String getApmProf() {
+		return apmProf;
 	}
 
-	public String getApm_prof() {
-		return apm_prof;
-	}
-
-	public void setApm_prof(String apm_prof) {
-		this.apm_prof = apm_prof;
+	public void setApmProf(String apmProf) {
+		this.apmProf = apmProf;
 	}
 
 	public int getEdad() {
@@ -88,12 +84,12 @@ public class Profesor {
 		this.edad = edad;
 	}
 
-	public String getSexo_prof() {
-		return sexo_prof;
+	public String getSexoProf() {
+		return sexoProf;
 	}
 
-	public void setSexo_prof(String sexo_prof) {
-		this.sexo_prof = sexo_prof;
+	public void setSexoProf(String sexoProf) {
+		this.sexoProf = sexoProf;
 	}
 
 	public String getRfc() {
@@ -104,14 +100,18 @@ public class Profesor {
 		this.rfc = rfc;
 	}
 
-	public int getSueldo_prof() {
-		return sueldo_prof;
+	public int getSueldoProf() {
+		return sueldoProf;
 	}
 
-	public void setSueldo_prof(int sueldo_prof) {
-		this.sueldo_prof = sueldo_prof;
+	public void setSueldoProf(int sueldoProf) {
+		this.sueldoProf = sueldoProf;
 	}
-	
+
+	public void setIdProf(int idProf) {
+		this.idProf = idProf;
+	}
+
 	
 	
 
