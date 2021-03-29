41731f24c1cba7ab75dd451c25b57c35f9e9b7f7
Geovanni Perez <ivan.gpv@gmail.com>
2021-03-29 14:55:15 -0600

    Cambios Alumno

--- MontessoriSchool/src/main/java/com/montessori/repository/AlumnoRepository.java
+++ MontessoriSchool/src/main/java/com/montessori/repository/AlumnoRepository.java
@@ -1,9 +1,11 @@
 package com.montessori.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.montessori.model.Alumno;
 
+@Repository
 public interface AlumnoRepository extends JpaRepository<Alumno, Integer>{
 
 }
