84b6fc6231999aca106e408c9d45d19d2844083d
Jose Francisco JFPS. Patino Solis <jose.patino@stkrursus.com>
2021-03-29 12:13:26 -0600

    Subo el proyecto

---
+++ MontessoriSchool/src/main/java/com/montessori/MontessoriSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.montessori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontessoriSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontessoriSchoolApplication.class, args);
+	}
+
+}
---
+++ MontessoriSchool/src/test/java/com/montessori/MontessoriSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montessori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontessoriSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
